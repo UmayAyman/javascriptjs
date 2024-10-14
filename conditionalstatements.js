5f7c60d68d3511cd2dd3b710dd3ecec2fe1790de
UmayAyman <aimanumme827@gmail.com>
2024-10-14 17:51:34 +0500

    conditional statements

---
+++ conditionalstatements.js
@@ -0,0 +1,50 @@
+//Arthemetic operations
+//let a = 5;
+//let b = 2;
+
+//console.log ("a = ", a , " & b = ", b );
+//console.log ("a + b = ", a + b);
+//console.log ("a - b = ", a - b);
+//console.log ("a * b = ", a * b);
+//console.log ("a / b = ", a / b);
+//console.log ("a % b = ", a % b);    //remainder
+//console.log ("a ** b = ", a ** b);  //5^2
+
+//Unary Operator
+let x = 5;
+let y = 2;
+
+console.log ("a = ", a , " & b = ", b );
+a = a + 1; //or we can write a++ instead of a = a + 1;
+console.log ("a = " , a); //6
+
+//a = a - 1; //or we can write a-- instead of a = a - 1;
+//console.log ("a = " , a); //4
+
+let z = 5;
+let q = 2;
+
+console.log ("z = ", z , " & q = ", q );
+console.log ("++z = " , ++z); //means it will simply print value and then change that after that we don't have to write console.log ("z = ", z); again
+//console.log ("z++ = " , z++); //means first it will change value than print
+console.log ("z = " , z);
+
+//Assignment operators
+//a += 4; // a = a + 4;
+//a -= 4; // a = a - 4;
+//a *= 4; // a = a * 4;
+//a /= 4; // a = a / 4;
+//a %= 4; // a = a % 4;
+//a **= 4; // a = a ** 4;
+
+//Comparison operator
+// == (equal to)
+// != (not equal to)
+// === (equal to and type)
+// !== (not equal to and type)
+// >, >=, <, <= ( greater than, less than, greater than or equal to, less than or equal to)
+
+//Logical Operators
+// && (AND)
+// || (OR)
+// ! (logical NOT)
